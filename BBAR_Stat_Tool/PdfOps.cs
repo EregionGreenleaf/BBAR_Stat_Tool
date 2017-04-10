d0226e903e5563bf9e7ef50c87581eeb85d6e747
Eregion <Eregion@ERIADOR>
2017-04-10 21:31:49 +0200

    ADDED:
    - PdfOps class

---
+++ BBAR_Stat_Tool/PdfOps.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBAR_Stat_Tool
+{
+    class PdfOps
+    {
+    }
+}
